12b3ebba1d0b36d062e0fe7e408e067b8f6514e0
Bastien <bas.des2005@gmail.com>
2026-01-29 11:49:19 +0100

    test interface

--- interface_main.java
+++ interface_main.java
@@ -1,12 +1,41 @@
 import  javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javax.swing.border.Border;
 
 public class interface_main extends Application {
 
     @Override
     public void start(Stage stage) {
-        stage.setTitle("Hello JavaFX");
-        stage.show();
+            BorderPane root = new BorderPane();
+            
+            /*Label etiquette = new Label ( " Je suis un label textuel " );
+            etiquette . setFont ( Font . font ( " Cambria " , 18));
+            etiquette . setTextFill ( Color . DARKCYAN );
+            root . setTop ( etiquette );*/
+
+            Image image = new Image("https://www.meme-arsenal.com/memes/7de1cccd58be2b01261e613c08663e9a.jpg");
+            ImageView imageView = new ImageView(image);
+            root . setCenter ( new Button ( " Commencer "));
+            // ajout du c ont eneur comme racine
+            imageView.setFitWidth(250);
+            imageView.setPreserveRatio(true);
+            Label labelAvecImage = new Label ();
+            labelAvecImage . setGraphic (imageView);
+
+            root.setTop ( labelAvecImage );
+
+            Scene scene = new Scene ( root, 1000, 800 );
+            stage . setTitle ( " Ma fenetre " );
+            stage . setScene ( scene );
+            stage . show ();
     }
 
     public static void main(String[] args) {
