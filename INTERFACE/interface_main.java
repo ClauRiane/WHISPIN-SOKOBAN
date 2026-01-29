1a51279a1d297b182aba33b5d9e3638cffe721f5
Bastien <bas.des2005@gmail.com>
2026-01-29 13:03:43 +0100

    Interface menu principal

--- INTERFACE/interface_main.java
+++ INTERFACE/interface_main.java
@@ -2,37 +2,42 @@ import  javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.geometry.Pos;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javax.swing.border.Border;
+import javafx.scene.shape.Rectangle;
 
 public class interface_main extends Application {
 
     @Override
     public void start(Stage stage) {
             BorderPane root = new BorderPane();
-            
-            /*Label etiquette = new Label ( " Je suis un label textuel " );
-            etiquette . setFont ( Font . font ( " Cambria " , 18));
-            etiquette . setTextFill ( Color . DARKCYAN );
-            root . setTop ( etiquette );*/
 
             Image image = new Image("https://www.meme-arsenal.com/memes/7de1cccd58be2b01261e613c08663e9a.jpg");
             ImageView imageView = new ImageView(image);
-            root . setCenter ( new Button ( " Commencer "));
-            // ajout du c ont eneur comme racine
             imageView.setFitWidth(250);
             imageView.setPreserveRatio(true);
+
             Label labelAvecImage = new Label ();
-            labelAvecImage . setGraphic (imageView);
+            labelAvecImage.setGraphic(imageView);
+            labelAvecImage.setStyle("-fx-background-color: transparent;");
+            
+            Button bouton = new Button("Commencer");
 
-            root.setTop ( labelAvecImage );
+            root.setAlignment( labelAvecImage, Pos.TOP_CENTER);
+            root.setTop( labelAvecImage );
+            root.setCenter(bouton);
+            
+            StackPane stack = new StackPane();
+            Rectangle fond = new Rectangle(1000, 800,Color.LIGHTGREEN);
+            stack.getChildren().addAll(fond, root);
 
-            Scene scene = new Scene ( root, 1000, 800 );
+            Scene scene = new Scene ( stack, 1000, 800);
+            
             stage . setTitle ( " Ma fenetre " );
             stage . setScene ( scene );
             stage . show ();
