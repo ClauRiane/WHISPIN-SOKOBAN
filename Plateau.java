90f422a317314e7dd0d1c6b8dbcf5f26e2783d0b
Yayamsss <nemay2005@gmail.com>
2026-04-27 16:39:19 +0200

    regle bug cible qui disparait

--- Plateau.java
+++ Plateau.java
@@ -285,7 +285,7 @@ public class Plateau {
             setCase(posApresBoite, nouvelleBoite);
             
             // Le personnage prend la place de la boite
-            boolean personnageSurCible = prochaineCase.estCible();
+            boolean personnageSurCible = boiteEtaitSurCible;
             Personnage nouveauPersonnage = new Personnage(personnageSurCible);
             setCase(prochainePos, nouveauPersonnage);
             
